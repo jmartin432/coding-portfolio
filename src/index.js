3d3dd763e244134b9e7835dd1c1a9d035a822191
Justin <jlmartin432@gmail.com>
2020-07-14 20:44:35 -0700

    basic web audio working

--- src/index.js
+++ src/index.js
@@ -20,18 +20,90 @@ function changeFont () {
   }
 }
 
-document.addEventListener('load', changeFont());
 
+const AudioContext = window.AudioContext || window.webkitAudioContext
+let noAudioButton
+let yesAudioButton
+let modalContainer
+let navButton1, navButton2, navButton3
+let audioContext
+let audioElement1, audioElement2, audioElement3
+let track1, track2, track3
+let gainNode
+let audio = false
 
+window.onload = (event) => {
+  changeFont()
+  noAudioButton = document.getElementById('no-audio-button')
+  yesAudioButton = document.getElementById('yes-audio-button')
+  modalContainer = document.getElementById('modal-container')
+  navButton1 = document.getElementById('nav-b1')
+  navButton2 = document.getElementById('nav-b2')
+  navButton3 = document.getElementById('nav-b3')
+  audioElement1 = document.getElementById('audio1')
+  audioElement2 = document.getElementById('audio2')
+  audioElement3 = document.getElementById('audio3')
 
-    // font-family: "'Odibee Sans', cursive;"
-    //
-    // font-family: 'Slabo 27px', serif;
-    //
-    // font-family: 'Fjalla One', sans-serif;
-    //
-    // font-family: 'Arvo', serif;
-    //
-    // font-family: 'Indie Flower', cursive;
+  noAudioButton.addEventListener('click', function () {
+    modalContainer.style.display = 'none'
+    audio = false
+    console.log(audio)
+  })
+  yesAudioButton.addEventListener('click', function () {
+    modalContainer.style.display = 'none'
+    audio = true
+    audioContext = new AudioContext()
+    console.log(audioContext)
+    gainNode = audioContext.createGain()
+    gainNode.connect(audioContext.destination)
+    track1 = audioContext.createMediaElementSource(audioElement1)
+    track1.connect(gainNode)
+    track2 = audioContext.createMediaElementSource(audioElement2)
+    track2.connect(gainNode)
+    track3 = audioContext.createMediaElementSource(audioElement3)
+    track3.connect(gainNode)
+  })
+
+  navButton1.addEventListener('mouseenter', function (event) {
+    if (audio) {
+      gainNode.gain.linearRampToValueAtTime(1.0, audioContext.currentTime + 0.2)
+      audioElement1.play()
+    }
+  })
+  navButton1.addEventListener('mouseout', function (event) {
+    if (audio) {
+      gainNode.gain.linearRampToValueAtTime(0.0, audioContext.currentTime + 0.2)
+      audioElement1.pause(0.2)
+      audioElement1.currentTime = 0.0
+    }
+  })
+  navButton2.addEventListener('mouseenter', function (event) {
+    if (audio) {
+      gainNode.gain.linearRampToValueAtTime(1.0, audioContext.currentTime + 0.2)
+      audioElement2.play()
+    }
+  })
+  navButton2.addEventListener('mouseout', function (event) {
+    if (audio) {
+      gainNode.gain.linearRampToValueAtTime(0.0, audioContext.currentTime + 0.2)
+      audioElement2.pause(0.2)
+      audioElement2.currentTime = 0.0
+    }
+  })
+  navButton3.addEventListener('mouseenter', function (event) {
+    if (audio) {
+      gainNode.gain.linearRampToValueAtTime(1.0, audioContext.currentTime + 0.2)
+      audioElement3.play()
+    }
+  })
+  navButton3.addEventListener('mouseout', function (event) {
+    if (audio) {
+      gainNode.gain.linearRampToValueAtTime(0.0, audioContext.currentTime + 0.2)
+      audioElement3.pause(0.2)
+      audioElement3.currentTime = 0.0
+    }
+  })
+}
+
 
 
